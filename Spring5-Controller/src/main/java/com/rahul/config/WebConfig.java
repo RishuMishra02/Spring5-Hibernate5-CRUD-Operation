ede50d64f28eab1f68691c6f9f250c661ee6d4aa
RishuMishra02 <er.rahulmishra02@gmail.com>
2018-11-03 15:16:33 -0400

    some changes in web file

--- Spring5-Controller/src/main/java/com/rahul/config/WebConfig.java
+++ Spring5-Controller/src/main/java/com/rahul/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.rahul.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/jsp/", ".jsp").getClass();

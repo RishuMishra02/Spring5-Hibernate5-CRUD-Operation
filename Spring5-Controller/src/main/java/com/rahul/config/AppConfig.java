e67d7489dffda713c797c1720c066e01fb3957fd
RishuMishra02 <er.rahulmishra02@gmail.com>
2018-11-03 15:25:26 -0400

    App changes

--- Spring5-Controller/src/main/java/com/rahul/config/AppConfig.java
+++ Spring5-Controller/src/main/java/com/rahul/config/AppConfig.java
@@ -2,9 +2,10 @@ package com.rahul.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
-
+@RequestScope
 @ComponentScan(basePackages = "com.rahul")
 public class AppConfig {
 

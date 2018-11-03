82e35445451603d71c8f7af5eb480209a6ce941c
RishuMishra02 <er.rahulmishra02@gmail.com>
2018-11-03 14:46:30 -0400

    changes in appconfig
    Change-Id: I0000000000000000000000000000000000000001
    Signed-off-by: RishuMishra02 <er.rahulmishra02@gmail.com>

--- Spring5-Controller/src/main/java/com/rahul/config/AppConfig.java
+++ Spring5-Controller/src/main/java/com/rahul/config/AppConfig.java
@@ -1,5 +1,12 @@
 package com.rahul.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.rahul")
 public class AppConfig {
 
 }
